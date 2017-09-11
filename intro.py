a1e22a9a00debaca6bc9ee28ca978dd068da4c85
Sumaira Zaman <sumaira.zaman10@gmail.com>
2017-09-11 18:37:03 -0400

    Add files via upload
    Introducing Python

---
+++ intro.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 18:12:24 2017
+
+@author: wegrzynlab
+"""
+
+###declaring variables
+class_number = 5300
+print "class number is: ", class_number
+class_name = "practical genomics"
+print "class name is: ", class_name
+students = ["Tom", "Sarah", "Anne", "Bob"] #list
+print "students in the class are: ", students
+grades = {86, 90, 84, 95} #array
+print "student grades are: ", grades
+info = (["Masters", 1], "phD", ["phD"], "Masters") #tuple
+print info
+
+###playing with lists
+
+
+###playing with strings
+student = "Sarah"
+print student
+print len(student)
+print student[0]
+print student[0]+student[1]+student[2]+student[3]+student[4]

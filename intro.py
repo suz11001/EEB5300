3340d1b97fd08f0b79dcb963447846695016d205
Sumaira Zaman <sumaira.zaman10@gmail.com>
2017-09-12 12:04:43 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,24 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 11 18:12:24 2017
-
-@author: wegrzynlab
-"""
-
 ###declaring variables
 class_number = 5300
 print "class number is: ", class_number
 class_name = "practical genomics"
 print "class name is: ", class_name
-students = ["Tom", "Sarah", "Anne", "Bob"] #list
+students = ["Tom", "Sarah", "Anne", "Bob"] #list of strings
 print "students in the class are: ", students
-grades = {86, 90, 84, 95} #array
+grades = [86, 90, 84, 95] #list of integers
 print "student grades are: ", grades
 info = (["Masters", 1], "phD", ["phD"], "Masters") #tuple
 print info
 
 ###playing with lists
+print students[0]
+print students[-1]
+print students[-2]
+print students[-5]
+print students[::1]
+print students[::-1]
+print students[3:0:-1]
+print students[3::-1]
 
+index = 0
+while index < len(students):
+  print index
+  print students[index]
+  index = index + 1
+  
+for s in students:
+  print s
+
+a=range(10)
+a=range(1,10+1)
+a=range(2,10+1,2)
 
 ###playing with strings
 student = "Sarah"
